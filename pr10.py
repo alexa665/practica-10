0993f9fb469d0b24fed25fbe373dc28d8abd2e52
alexa665 <a.23308051281205@cbtis128.edu.mx>
2024-09-24 20:47:46 -0600

    Add files via upload

---
+++ pr10.py
@@ -0,0 +1,11 @@
+print(" ")
+print("alexa guadalupe ramirez manzo")
+print(" ")
+print("programa que solisita los kilometros y litros de gasolina")#nombre del programa
+print(" ")
+k=float(input("ingresa los kilometros")) #variable, linea para ingresar los kilometros
+l=float(input("ingresa los litros de gasolina")) #variable, linea para ingresar los litros de gasolina
+print(" ")
+div=l/k #linea de division
+print("el resultado de la division es",div) #inpresion de la division
+print(" ")
